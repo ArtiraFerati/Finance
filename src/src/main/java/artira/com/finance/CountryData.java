fd7faf06a214901c0ef258688129040b882f4ab4
ArtiraFerati <artiraferati@gmail.com>
2019-06-12 20:51:48 +0200

    Add files via upload

---
+++ src/src/main/java/artira/com/finance/CountryData.java
@@ -0,0 +1,8 @@
+package artira.com.finance;
+
+public class CountryData {
+
+    public static final String[] countryNames = {"Afganistan", "Albania", "Algeria", "Andorra", "Angola", "Antarctica", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Kosovo"};
+
+    public static final String[] countryAreaCodes = {"93", "355", "213", "376", "244",  "672", "54", "374", "297", "61", "43", "994", "377"};
+}
